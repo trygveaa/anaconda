7bf9da83fd09b83d7124eb377eae963323832417
Trygve Aaberge <trygveaa@gmail.com>
2013-11-29 15:19:38 +0100

    Add In_reply_to_status_id to Tweet struct

--- tweet.go
+++ tweet.go
@@ -1,15 +1,17 @@
 package anaconda
 
 type Tweet struct {
-	Source        string
-	Id            int64
-	Retweeted     bool
-	Favorited     bool
-	User          TwitterUser
-	Truncated     bool
-	Text          string
-	Retweet_count int64
-	Id_str        string
-	Created_at    string
-	Entities      TwitterEntities
+	Source                    string
+	Id                        int64
+	Retweeted                 bool
+	Favorited                 bool
+	User                      TwitterUser
+	Truncated                 bool
+	Text                      string
+	Retweet_count             int64
+	Id_str                    string
+	Created_at                string
+	Entities                  TwitterEntities
+	In_reply_to_status_id     *int64
+	In_reply_to_status_id_str *string
 }
